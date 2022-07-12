ff27fdc2e4fa2318f65611b3ef16493cf3fc82cd
Jeremias Mane <jmane@magnasistemas.com.br>
2022-07-12 11:26:37 -0300

    feignClient

--- hr-payrool/src/main/java/com/devsuperior/hrpayrool/HrPayroolApplication.java
+++ hr-payrool/src/main/java/com/devsuperior/hrpayrool/HrPayroolApplication.java
@@ -2,8 +2,10 @@ package com.devsuperior.hrpayrool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class HrPayroolApplication {
 
 	public static void main(String[] args) {
