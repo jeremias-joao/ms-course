6c3d03086ca12e7b6172c746d29ebe20168120d8
Jeremias Mane <jmane@magnasistemas.com.br>
2022-07-13 16:46:41 -0300

    refatoring project in general

--- hr-worker/src/main/java/com/devsuperior/hrworker/repositories/WorkerRepository.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/repositories/WorkerRepository.java
@@ -1,11 +1,9 @@
 package com.devsuperior.hrworker.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.devsuperior.hrworker.entities.Worker;
 
-@Repository
-public interface WorkerRepository   extends JpaRepository<Worker, Long>{
+public interface WorkerRepository extends JpaRepository<Worker, Long> {
 
 }
