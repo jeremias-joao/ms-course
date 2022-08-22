2a717c1e2a81799178269ebd870ae7391c2de53c
Jeremias Mane <jmane@magnasistemas.com.br>
2022-08-22 15:12:38 -0300

    github config

--- hr-oauth/src/main/java/com/devsuperior/hroauth/entities/User.java
+++ hr-oauth/src/main/java/com/devsuperior/hroauth/entities/User.java
@@ -1,16 +1,10 @@
 package com.devsuperior.hroauth.entities;
 
 import java.io.Serializable;
-import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
-import java.util.stream.Collectors;
 
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
-public class User implements UserDetails, Serializable {
+public class User implements /*UserDetails,*/ Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private Long id;
@@ -92,7 +86,7 @@ public class User implements UserDetails, Serializable {
 		return true;
 	}
 
-	@Override
+	/*@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
 		return roles.stream().map(x -> new SimpleGrantedAuthority(x.getRoleName()))
 				.collect(Collectors.toList());
@@ -121,5 +115,5 @@ public class User implements UserDetails, Serializable {
 	@Override
 	public boolean isEnabled() {
 		return true;
-	}
+	}*/
 }
